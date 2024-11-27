233a1e57379cd83ce6aea7c8c1e86a54e12cc8f2
zerothehero15 <138108725+zerothehero15@users.noreply.github.com>
2024-11-26 16:35:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,13 @@ except Exception as e:
 
 @app.route('/')
 def home():
-    return render_template('templatesindex.html')
+    return render_template('index.html')
+
 
 @app.route('/predict', methods=['POST'])
 def predict():
     if model is None:
-        return render_template('templatesindex.html', prediction_text="Error: Model not loaded!")
+        return render_template('index.html', prediction_text="Error: Model not loaded!")
 
     # Get input data from the form
     size = float(request.form['size'])
@@ -34,7 +35,7 @@ def predict():
     # Predict the price
     prediction = model.predict(input_features)[0]
 
-    return render_template('templatesindex.html', prediction_text=f'Predicted House Price: ${prediction:,.2f}')
+    return render_template('index.html', prediction_text=f'Predicted House Price: ${prediction:,.2f}')
 
 if __name__ == "__main__":
     app.run(debug=True)
